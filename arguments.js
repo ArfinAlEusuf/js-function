b62c51f5f6e01b89447f0a6d6384017352a2276d
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-07-31 10:58:30 +0600

    double

---
+++ arguments.js
@@ -0,0 +1,12 @@
+function doubleIt(number){
+    const doubled = number *2;
+    console.log(number, doubled);
+}
+
+console.log('i will call the function')
+doubleIt(15);
+console.log("-------------");
+doubleIt(55);
+console.log("-------------");
+doubleIt(48);
+console.log("-------------");
