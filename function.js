206690e0eed3049931a847b1bba3253e050b7fdf
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-07-31 10:25:33 +0600

    intro of function

---
+++ function.js
@@ -0,0 +1,26 @@
+const number= 45;
+
+function fanOffKor(){
+    console.log('bosha theke uthe dara');
+    console.log('wolk towards the switch');
+    console.log('click the switch to off of the fan');
+}
+
+/**
+ * add function
+function name(){
+}
+ */
+
+
+function brushTeath(){
+    console.log('pick up the brush');
+    console.log('add paste');
+    console.log('2 min dhore dath majhte thako')
+}
+
+/**
+ * call the function
+ * name();
+ */
+brushTeath();
