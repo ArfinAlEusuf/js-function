c44f98a39914e519f06939521983229289f518c6
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-07-31 10:53:43 +0600

    add all

--- parameter.js
+++ parameter.js
@@ -18,16 +18,23 @@ function square (number){
     console.log('square of the given number is: ', borgo);
 }
 
-square(4);
-console.log('-----------');
-square(12);
-square(5);
-square(405);
+// square(4);
+// console.log('-----------');
+// square(12);
+// square(5);
+// square(405);
 
 function add(num1,num2){
     const sum = num1 + num2;
     console.log(sum);
 }
 
-add(5,71);
-add(65.31);
+// add(5,71);
+add(65,31);
+
+function addAll(a,b,c,d,e){
+    const total=  a+b+c+d+e;
+    console.log(total)
+}
+
+addAll(3,4,5,6,7);
