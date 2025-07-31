f816fc1344e6d3e0f70df151dbfdac363b61edc9
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-07-31 11:07:28 +0600

    difference

--- arguments.js
+++ arguments.js
@@ -14,3 +14,11 @@ console.log("-------------");
 const number=  12
 doubleIt(number)
 
+function difference (num1 , num2){
+    const diff = num1 - num2;
+    console.log(num1,num2, 'difference is :', diff);
+}
+
+const fatherAge=40;
+const myAge = 10;
+difference(fatherAge, myAge);
