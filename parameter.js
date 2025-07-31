d8fe860a5542f7683cef7316d9816876ac226ecd
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-07-31 10:42:45 +0600

    get a parameter from function

---
+++ parameter.js
@@ -0,0 +1,20 @@
+function vaatKhao(){
+    console.log('hand wash kore asho');
+    console.log('boso');
+    console.log('plate e khabar dabar nao');
+    console.group('gopat gopat kore khao');
+}
+
+
+// vaatKhao();
+
+//  3---> 3*3 = 9
+// 4 ---> 4*4 = 16
+//  11 ---> 11*11 = 121
+
+function square (number){
+    console.log(number);
+    console.log('value of the number parameter', number)
+}
+
+square(4);
