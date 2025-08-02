2fa942789ecb492a7895e4b18820c96b15c91ad6
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-08-02 23:30:31 +0600

    sum of odd numbers

--- even-only.js
+++ even-only.js
@@ -14,5 +14,20 @@ function evenNumbersOnly(numbers){
 }
 
 const numbers = [5,8,91,24,6]
-const evens = evenNumbersOnly(numbers);
-console.log('enen numbers are:',evens)
+// const evens = evenNumbersOnly(numbers);
+// console.log('enen numbers are:',evens)
+
+// sum of even numbers
+
+function sumOfEvenNumbers(numbers){
+    let total = 0; 
+    for(const num of numbers){
+        if(num % 2===0){
+            total= total + num;
+        }
+    }
+    return total;
+}
+const num= [1,5,6,8,3,12,16,18];
+const total = sumOfEvenNumbers(num);
+console.log('total of even numbers is: ',total)
