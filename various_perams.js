848e19670116dba5e628c0c854888a4e94773600
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-08-02 21:09:20 +0600

    use string as perameter

---
+++ various_perams.js
@@ -0,0 +1,11 @@
+/**
+ * for a given string tell me whether it has ecen numbers or not
+ * 
+ */
+
+function evenSizedString(str){
+    const size= str.length;
+    console.log(str);
+}
+
+evenSizedString("Dhaka")
