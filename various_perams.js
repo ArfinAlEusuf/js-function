d523172cdad81a141d15cdbb628d692374b4885c
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-08-02 22:55:53 +0600

    multipule function

--- various_perams.js
+++ various_perams.js
@@ -5,7 +5,21 @@
 
 function evenSizedString(str){
     const size= str.length;
-    console.log(str);
+    console.log(str,size);
 }
 
-evenSizedString("Dhaka")
+evenSizedString("Dhaka");
+
+function doubleOrTriple(number, doDouble){
+    if(doDouble=== true){
+        const result= number * 2;
+        return result;
+    }
+    else{
+        const result = number * 3;
+        return result;
+    }
+}
+
+console.log(doubleOrTriple(5,true));
+console.log(doubleOrTriple(4,false));
