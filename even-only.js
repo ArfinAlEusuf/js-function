218907e5b59b07fdbb2e9661dd792735ed26222a
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-08-02 23:14:18 +0600

    getting numbers

---
+++ even-only.js
@@ -0,0 +1,12 @@
+/**
+ * create function that will return only the even numbers
+ */
+
+function evenNumbersOnly(numbers){
+    for(const number of numbers){
+        console.log(numbers);
+    }
+}
+
+const numbers = [5,8,91,24,6]
+evenNumbersOnly(numbers);
